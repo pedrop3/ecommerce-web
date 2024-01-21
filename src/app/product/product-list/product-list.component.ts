df53b4b606dbcb3d87a1b2df2d79a1b194321417
Pedro Santos <pedro.santos@imedicina.com.br>
2024-01-21 01:03:06 +0000

    adding list products

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductService } from '../product.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.productService.getProducts().pipe(
+      map((products: Product[]) => {
+        this.products = products;
+      })
+    ).subscribe();
+
+
+  }
 
 }
