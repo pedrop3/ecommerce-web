d567d29d02c7f5b1b910320836c057d9b2b17772
Pedro Santos <pedro.santos@imedicina.com.br>
2024-01-21 00:50:40 +0000

    Creating the service products

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment as env } from 'src/environments/environment';
+import { Product } from '../models/product';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = env.apiUrl + '/products'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.apiUrl);
+  }
+
+
 }
